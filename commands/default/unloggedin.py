252b62ff6cc74335aed8c474d4ecc54df6b23db8
Julia Willson <julia.willson@gmail.com>
2017-12-13 21:07:48 -0800

    core - resolves #15 | No WHO screen when not logged in yet.

--- commands/default/unloggedin.py
+++ commands/default/unloggedin.py
@@ -2,6 +2,8 @@
 Commands that are available from the connect screen.
 """
 from evennia.commands.default.unloggedin import CmdUnconnectedHelp
+import types
+from evennia.commands.default.account import CmdWho
 
 
 class CmdUnconnectedHelp(CmdUnconnectedHelp):
@@ -15,6 +17,7 @@ You are not yet logged into the game. Commands available at this point:
   |wcreate|n - create a new |wOOC|n account
   |wconnect|n - connect with an existing account
   |wlook|n - re-show the connection screen
+  |wwho|n - show which players are connected
   |whelp|n - show this help
   |wencoding|n - change the text encoding to match your client
   |wscreenreader|n - make the server more suitable for use with screen readers
@@ -28,3 +31,37 @@ You can use the |wlook|n command if you want to see the connect screen again.
 
 """
         self.caller.msg(string)
+
+
+class CmdSessionWho(CmdWho):
+    __doc__ = CmdWho.__doc__
+
+    class PartialMockAccount(object):
+        """
+        Partially mocked singleton, so it is kept here.
+        """
+        def __new__(cls):
+            if not hasattr(cls, 'instance'):
+                cls.instance = super(CmdSessionWho.PartialMockAccount, cls).__new__(cls)
+
+            return cls.instance
+
+        def check_permstring(self, permission=None):
+            return False
+
+    @staticmethod
+    def _session_msg_wrapper(target, msg=None):
+        if msg:
+            target.caller.msg(msg)
+
+    def func(self):
+        """
+        Get all connected accounts by polling session.
+        """
+
+        if not self.account:
+            # This is a session.
+            self.msg = types.MethodType(CmdSessionWho._session_msg_wrapper, self)
+            self.account = self.PartialMockAccount()
+
+        super(CmdSessionWho, self).func()
